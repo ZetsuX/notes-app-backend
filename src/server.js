76f3a33075020fe021a7858ff3d18b13fbb7c850
Fehlling Halim <kevnhalim@gmail.com>
2023-04-18 19:23:03 +0700

    refactor: use onPreResponse for error handling

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const Hapi = require("@hapi/hapi");
 const notes = require("./api/notes");
 const NotesService = require("./services/postgres/NotesService");
 const NotesValidator = require("./validator/notes");
+const ClientError = require("./exceptions/ClientError");
 
 const init = async () => {
     const notesService = new NotesService();
@@ -24,6 +25,33 @@ const init = async () => {
             service: notesService,
             validator: NotesValidator,
         },
+    });
+
+    server.ext("onPreResponse", (request, h) => {
+        const { response } = request;
+        if (response instanceof Error) {
+            if (response instanceof ClientError) {
+                const newResponse = h.response({
+                    status: "fail",
+                    message: response.message,
+                });
+                newResponse.code(response.statusCode);
+                return newResponse;
+            }
+
+            if (!response.isServer) {
+                return h.continue;
+            }
+
+            const newResponse = h.response({
+                status: "error",
+                message: "terjadi kegagalan pada server kami",
+            });
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        return h.continue;
     });
 
     await server.start();
