f4645f8c6e37d8b560dfef7fb7acbc1fb8f2b7fc
Fehlling Halim <kevnhalim@gmail.com>
2023-04-19 15:52:09 +0700

    fix: adjust users service

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -1,8 +1,8 @@
+const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
 const InvariantError = require("../../exceptions/InvariantError");
 const NotFoundError = require("../../exceptions/NotFoundError");
-const { nanoid } = require("nanoid");
-const bcrypt = require("bcrypt");
 
 class UsersService {
     constructor() {
@@ -10,31 +10,32 @@ class UsersService {
     }
 
     async addUser({ username, password, fullname }) {
-        await this.verifyUsername(username);
+        await this.verifyNewUsername(username);
 
         const id = `user-${nanoid(16)}`;
         const hashedPassword = await bcrypt.hash(password, 10);
 
         const query = {
             text: "INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id",
-            value: [id, username, hashedPassword, fullname],
+            values: [id, username, hashedPassword, fullname],
         };
 
         const result = await this._pool.query(query);
+
         if (!result.rows.length) {
             throw new InvariantError("User gagal ditambahkan");
         }
-
         return result.rows[0].id;
     }
 
-    async verifyUsername(username) {
+    async verifyNewUsername(username) {
         const query = {
-            text: "SELECT * FROM users WHERE username = $1",
-            value: [username],
+            text: "SELECT username FROM users WHERE username = $1",
+            values: [username],
         };
 
         const result = await this._pool.query(query);
+
         if (result.rows.length > 0) {
             throw new InvariantError(
                 "Gagal menambahkan user. Username sudah digunakan."
