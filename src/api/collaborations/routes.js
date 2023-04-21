7d0fa58f06ef7c2366fb78c72eda5d2be0f2166b
Fehlling Halim <kevnhalim@gmail.com>
2023-04-21 17:44:59 +0700

    feat: add collaborations validator and plugin

---
+++ src/api/collaborations/routes.js
@@ -0,0 +1,21 @@
+const routes = (handler) => [
+    {
+        method: "POST",
+        path: "/collaborations",
+        handler: (request, h) => handler.postCollaborationHandler(request, h),
+        options: {
+            auth: "notesapp_jwt",
+        },
+    },
+
+    {
+        method: "DELETE",
+        path: "/collaborations",
+        handler: (request) => handler.deleteCollaborationHandler(request),
+        options: {
+            auth: "notesapp_jwt",
+        },
+    },
+];
+
+module.exports = routes;
